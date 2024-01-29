af8373063ed3d1321919fe01954b4a357f80b85e
klasick <olegfrolov6584@gmail.com>
2024-01-29 20:18:28 +0700

    домашнее задание

---
+++ proba/for.py
@@ -0,0 +1,5 @@
+my_cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+while cars_count < len(my_cars):
+    car = my_cars(cars_count)
+    print('Я езжу на автомабиле марки')
---
+++ proba/funkcia parametrov.py
@@ -0,0 +1,4 @@
+def print_params (name, age):
+    print('данные гостей', name, age)
+    return name, age
+name_list = ['Oleg', 'Liya', 'Alex']
